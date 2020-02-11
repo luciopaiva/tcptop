7f09f1b7d1d551c918ab0f1a57e194fe6d437f1a
Lucio Paiva <luciopaiva@gmail.com>
2020-02-11 14:51:34 +0000

    retries.sh: show only established connections

--- retries.sh
+++ retries.sh
@@ -8,4 +8,4 @@ fi
 COLUMNS="local_port,remote_addr,alias,state,backoff,rto,timer,send_queue,recv_queue,bytes_recv,unacked"
 COLUMNS="${COLUMNS},retrans_cur,retrans_total,mss,ssthresh,segs_in,segs_out,last_ack_human"
 
-watch -n1 "./tcptop -o lastack -c ${COLUMNS} ${PORT}"
+watch -n1 "./tcptop -s established -o lastack -c ${COLUMNS} ${PORT}"
