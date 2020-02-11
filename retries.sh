32ae73ae72f8f052315a390c38303253efb36e0e
Lucio Paiva <luciopaiva@gmail.com>
2020-02-11 00:06:38 +0000

    Script for retransmission investigation

---
+++ retries.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PORT=""
+if [ $# -eq 1 ]; then
+    PORT="-p $1"
+fi
+
+COLUMNS="local_port,remote_addr,alias,state,backoff,rto,timer,send_queue,recv_queue,bytes_recv,unacked"
+COLUMNS="${COLUMNS},retrans_cur,retrans_total,mss,ssthresh,segs_in,segs_out,last_ack_human"
+
+watch -n1 "./tcp-retries.py -o lastack -c ${COLUMNS} ${PORT}"
